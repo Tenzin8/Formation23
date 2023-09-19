e517a0a8e00a3570407cfcfa7e3f8da0916ac9b4
Tenzin KHANGSAR <tenzin.khangsar@viaformation.e-cdp.com>
2023-09-19 10:20:29 +0200

    Passage en syntaxe simplifiée

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,7 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
+﻿
         Console.WriteLine("Hello, World!");
         int A, B;
         A = 7;
         B = 5;
         A = B;
         B = A;
-    }
-}

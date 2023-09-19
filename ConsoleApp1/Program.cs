c2df70fb8e7c3dc680ec344a25812da51d0646a5
Tenzin KHANGSAR <tenzin.khangsar@viaformation.e-cdp.com>
2023-09-19 10:03:06 +0200

    commit initial

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        int A, B;
+        A = 7;
+        B = 5;
+        A = B;
+        B = A;
+    }
+}

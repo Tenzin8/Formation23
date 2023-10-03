7e963b9f9ecdc20a0f7374bf0df79634227e2f9f
Tenzin8 <dedentenzin61@gmail.com>
2023-10-03 10:28:07 +0200

    solution exercice

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿// Récupération du nombre
+Console.WriteLine("saisie un nombre");
+string saisie = Console.ReadLine();
+//conversion de string vers decimal
+decimal nombre = decimal.Parse(saisie);
+// affichage conditional
+if (nombre > 0)
+{
+    Console.WriteLine("le nombre est positive");
+}
+else if (nombre < 0)
+{
+    Console.WriteLine("le nombre est négative");
+}
+else
+{
+    Console.WriteLine ("le nombre est positif et négatif (c'est zéro)");
+}

f71418217877e36fcc78d54ee04192a7d14d27f2
Tenzin8 <dedentenzin61@gmail.com>
2023-10-10 09:40:39 +0200

    SOLUTION EX34

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿Console.WriteLine("insert a number");
+string saisaieNum = Console.ReadLine();
+int num = int.Parse(saisaieNum);
+if  (num < 0)
+{
+    Console.WriteLine("the number is negatif");
+}
+else if (num > 0)
+{
+    Console.WriteLine("the number is positif"); 
+}
+else 
+{
+    Console.WriteLine("the number is 0");
+}

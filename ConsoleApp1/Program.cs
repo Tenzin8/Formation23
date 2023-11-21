4750c4c18a02187640fa4fce5254eb708d54169c
Tenzin8 <dedentenzin61@gmail.com>
2023-11-21 11:28:34 +0100

    Solution tryparse

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,49 @@
-﻿// demander l'age de l'utilisateur
-Console.WriteLine(Quel age avez vous);
+﻿Console.WriteLine("Saisie votre Age : ");
+String saisieAge = Console.ReadLine();
+int Age = int.Parse(saisieAge);
+Console.WriteLine("Etes vous une fille ou un garçon?");
+Console.WriteLine("1 fille");
+Console.WriteLine("2 garçon");
+Console.WriteLine();
+Console.WriteLine("Selectionnez 1 ou 2 :");
+String saisieSexe = Console.ReadLine();
+int ChoixGenre = int.Parse(saisieSexe);
+string messageFinal = "Tu es ";
+
+if (ChoixGenre == 1 && Age < 18) 
+{
+    messageFinal = messageFinal + "mineure.";
+}
+else if (ChoixGenre == 1 && Age >= 18)
+{
+    messageFinal += "majeure.";
+}
+else if (ChoixGenre == 2 && Age < 18)
+{
+    messageFinal += "mineur.";
+}
+else if (ChoixGenre == 2 && Age >= 18)
+{
+    messageFinal += "majeur.";
+}
+else
+{
+    messageFinal = "problème !";
+}
+Console.WriteLine(messageFinal);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
